5d6d8e53a332d3f96a613d6dcbc15e2b3851c874
needfire <liuqianjiu@gmail.com>
2019-11-27 15:08:55 +0800

    更换默认管理员邮箱；允许注册

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // 管理员账号，管理员默认密码是 ymfe.org，不可配置，请安装完成后自行登录修改
-  adminAccount: 'admin@hello.yapi',
+  adminAccount: 'guofeng.liu@majorbio.com',
 
   // MongoDB 数据库配置
   db: {
@@ -25,7 +25,7 @@ module.exports = {
   },
 
   // 是否禁止注册
-  closeRegister: true,
+  closeRegister: false,
 
   // 需安装的插件列表
   plugins: [
